8ec819ae43f9da841a5f6bec3cced364b775c3f5
motaz99 <motaz.dn.ali@gmail.com>
2023-02-26 10:25:57 +0300

    feat(app.js): use morgan library

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -20,6 +25,8 @@ const tours = JSON.parse(
     `${__dirname}/dev-data/data/tours-simple.json`
   )
 );
+
+// 2) ROUTES HANDLER
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -108,6 +115,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -118,6 +126,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
