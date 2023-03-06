ea5cfaf2df9fe97f076f643ea8a7765e1d5f6c60
motaz99 <motaz.dn.ali@gmail.com>
2023-03-06 09:36:55 +0300

    feat: make the API better pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,7 +41,21 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
-    // SECOND WE EXECUTE THE QUERY
+    // 4) Pagination
+    // page2&limit=10
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours)
+        throw new Error('This page is not exist');
+    }
+
+    // EXECUTE THE QUERY
     const tours = await query;
 
     //SEND RESPONSE
@@ -54,7 +68,7 @@ exports.getAllTours = async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'fail',
       message: err,
     });
