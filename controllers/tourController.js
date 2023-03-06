a5c3f41d22df9d870fc4e9f9d91655cc85b4e7e4
motaz99 <motaz.dn.ali@gmail.com>
2023-03-06 16:17:17 +0300

    refactor: start using classes to clean up the code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,62 +1,17 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+
 
 exports.getAllTours = async (req, res) => {
   try {
-    // FIRST WE BUILD THE QUERY
-    // 1A) FILTERING
-    const queryObj = { ...req.query };
-    const excludedFields = [
-      'page',
-      'sort',
-      'limit',
-      'fields',
-    ];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 2B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(
-      /(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`
-    );
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy);
-      // if we want to sort depending on a second criteria we pass another argument to the sort func and also add that in the url with the ,  for ex: http://127.0.0.1:3000/api/v1/tours?sort=-price,ratingsAverage
-      // sort('price ratingsAverage')
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-    // page2&limit=10
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours)
-        throw new Error('This page is not exist');
-    }
-
     // EXECUTE THE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     //SEND RESPONSE
 
@@ -147,7 +102,7 @@ exports.deleteTour = async (req, res) => {
       data: null,
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'fail',
       message: 'Invalid data sent!',
     });
