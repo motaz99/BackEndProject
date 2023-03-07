0af069b2e807ac53082c2c047dcf326cd7cbfd16
motaz99 <motaz.dn.ali@gmail.com>
2023-03-07 13:10:16 +0300

    feat: aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -98,7 +98,15 @@ tourSchema.post(/^find/, function (docs, next) {
   console.log(
     `Query took ${Date.now() - this.start} milliseconds`
   );
-  console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+  console.log(this.pipeline());
   next();
 });
 
